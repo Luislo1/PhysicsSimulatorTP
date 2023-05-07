e3faee5c91cdf18c3866589a36be350e5d3c3220
LCM03 <lcabal03@ucm.es>
2023-05-07 14:04:56 +0200

    c

--- src/simulator/model/TempSennBody.java
+++ src/simulator/model/TempSennBody.java
@@ -6,9 +6,13 @@ public class TempSennBody extends Body {
 
 	public TempSennBody(String id, Vector2D v, Vector2D p, double m, double minT, double maxT, double redF, double incF) {
 		// minT(minimum temperature) and maxT(maximum temperature), both must be positive.
-		// redF(temperature reduction factor) and incF(temperature increase factor), both are between 0 and 1.
-		super(id, gid, v, p, m);
-		// TODO Auto-generated constructor stub
+		// redF(temperature reduction factor) and incF(temperature increase factor), both are between 0 and 1, both included.
+		super(id, gid, v, p, m); // TODO de donde saco el gid??
+		if (minT <= 0 || maxT <= 0 || redF < 0 || redF > 1 || incF < 0 || incF > 1) 
+			throw new IllegalArgumentException("Invalid parameters to create MTFP law");
+		this.g = g;
+		this.c = c;
+		
 	}
 
 	@Override
