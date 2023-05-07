0ffbdb0c36e8ca1cba6ff15a66c449fa7e0a070e
LCM03 <lcabal03@ucm.es>
2023-05-07 14:17:05 +0200

    c

---
+++ src/simulator/view/MovingAroundBodyGreaterMass.java
@@ -0,0 +1,24 @@
+package simulator.view;
+
+import java.util.List;
+
+import simulator.misc.Vector2D;
+import simulator.model.Body;
+import simulator.model.ForceLaws;
+
+public class MovingAroundBodyGreaterMass implements ForceLaws {
+	
+	public MovingAroundBodyGreaterMass(Vector2D c, double g)  { 
+		if (g <= 0 || c == null) 
+			throw new IllegalArgumentException("Invalid parameters to create MTFP law");
+		this.g = g;
+		this.c = c;
+	}
+
+	@Override
+	public void apply(List<Body> bs) {
+		
+		
+	}
+
+}
