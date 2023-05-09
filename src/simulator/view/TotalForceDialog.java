10a779aac0f7a86a0af48e400b7d660ba42711f1
LCM03 <lcabal03@ucm.es>
2023-05-09 10:45:39 +0200

    c

--- src/simulator/view/TotalForceDialog.java
+++ src/simulator/view/TotalForceDialog.java
@@ -28,7 +28,7 @@ import simulator.model.Body;
 import simulator.model.SimulatorObserver;
 
 @SuppressWarnings("serial")
-public class TotalForceDialog extends JDialog implements SimulatorObserver{
+public class TotalForceDialog extends JDialog {
 	String[] _headers = { "Id", "Total force" };
 	List<Body> _bodies;
 	private DefaultTableModel _dataTableModel;
@@ -48,128 +48,30 @@ public class TotalForceDialog extends JDialog implements SimulatorObserver{
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 		setContentPane(mainPanel);
 		
-		JPanel labelPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		mainPanel.add(labelPanel);
-		
 		JPanel eventsPanel = new JPanel(new BorderLayout());
 		mainPanel.add(eventsPanel, BorderLayout.CENTER);
 		
-		_dataTableModel = new DefaultTableModel() {
-			/*
-			@Override
-			public boolean isCellEditable(int row, int column) {
-				return (column == 0);// Make column 1 not editable
-			}
-			*/
-		};
-		_dataTableModel.setColumnIdentifiers(_headers);
-		JTable _dataTable = new JTable(_dataTableModel);
-		eventsPanel.add(_dataTable);
-		eventsPanel.add(new JScrollPane(_dataTable, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+		JTable _totalForceTable = new JTable(_forcesTableModel);
+		eventsPanel.add(_totalForceTable);
+		eventsPanel.add(new JScrollPane(_totalForceTable, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED));
-		TableColumn tableColumn = _dataTable.getColumnModel().getColumn(1);
-		tableColumn.setMinWidth(300);
-		
-		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.setAlignmentX(CENTER_ALIGNMENT);
-		mainPanel.add(buttonsPanel, BorderLayout.PAGE_END);
-
-		JButton okButton = new JButton("OK");
-		okButton.addActionListener((e) -> {
-			try {
-				// TODO que pasa si pulso ok????
-			} catch (Exception exc) {
-				Utils.showErrorMsg("Problem encountered with new force laws, enter a valid value");
-
-			}
-			//_status = 1;
-			//ForceLawsDialog.this.setVisible(false);
-		});
-		buttonsPanel.add(okButton);
-		
-		setPreferredSize(new Dimension(700, 400));
-		pack();
-		setResizable(false);
-		setVisible(false);
-		
-		/*
-		_forceLawsInfo = _ctrl.getForceLawsInfo(); // Get the list of available force laws with their info
-
-		JLabel labelIntro = new JLabel(
-				"<html>Select a force law and provide values of the parameters in the <font color = 'black'>Value column</font>(default values are used for parameters <br>with no value)</html>"); // <br>
-																																																	// =
-																																																	// lineBrak
-		labelIntro.setAlignmentX(Component.LEFT_ALIGNMENT);
-		labelIntro.setAlignmentY(Component.TOP_ALIGNMENT);
-		JPanel labelPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		labelPanel.add(labelIntro);
-		mainPanel.add(labelPanel);
-
-		JPanel eventsPanel = new JPanel(new BorderLayout());
-		mainPanel.add(eventsPanel, BorderLayout.CENTER);
-
-		_dataTableModel = new DefaultTableModel() {
-			@Override
-			public boolean isCellEditable(int row, int column) {
-				return (column == 1);// Make column 1 editable
-			}
-		};
-		_dataTableModel.setColumnIdentifiers(_headers);
-		JTable _dataTable = new JTable(_dataTableModel);
-		eventsPanel.add(_dataTable);
-		eventsPanel.add(new JScrollPane(_dataTable, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED));
-				
-		TableColumn tableColumn = _dataTable.getColumnModel().getColumn(2);
-		tableColumn.setMinWidth(300);
 		JPanel comboBoxPanel = new JPanel();
 		mainPanel.add(comboBoxPanel, BorderLayout.PAGE_END);
 
-		JLabel forceLawLabel = new JLabel("Force Law:");
-		comboBoxPanel.add(forceLawLabel);
-
-		_lawsModel = new DefaultComboBoxModel<>();
-		_laws = new JComboBox<>(_lawsModel);
-		_laws.addActionListener((e) -> { // Action listener to use the selected combo box index to fill the table
-			_selectedLawsIndex = _laws.getSelectedIndex();
-			fillInTable(_selectedLawsIndex);
-		});
-		comboBoxPanel.add(_laws);
-
-		JLabel groupLabel = new JLabel("Group:");
-		comboBoxPanel.add(groupLabel);
-
-		for (JSONObject j : _forceLawsInfo) // Fill in Force Laws ComboBox
-			_lawsModel.addElement(j.getString("desc"));
-
-		_groupsModel = new DefaultComboBoxModel<>();
-		_groups = new JComboBox<>(_groupsModel); // Groups ComboBox is filled in observer methods
-		comboBoxPanel.add(_groups);
-
 		JPanel buttonsPanel = new JPanel();
 		buttonsPanel.setAlignmentX(CENTER_ALIGNMENT);
 		mainPanel.add(buttonsPanel, BorderLayout.PAGE_END);
 
-		buttonsPanel.add(cancelButton);
-
 		JButton okButton = new JButton("OK");
 		okButton.addActionListener((e) -> {
-			try {
-				setNewForceLaws();
-			} catch (Exception exc) {
-				Utils.showErrorMsg("Problem encountered with new force laws, enter a valid value");
-
-			}
-			_status = 1;
-			ForceLawsDialog.this.setVisible(false);
+			TotalForceDialog.this.setVisible(false);
 		});
 		buttonsPanel.add(okButton);
 
 		setPreferredSize(new Dimension(700, 400));
 		pack();
 		setResizable(false);
-		setVisible(false);
-		*/
+		setVisible(true);
 	}
 	
 	public int open() {
@@ -185,46 +87,5 @@ public class TotalForceDialog extends JDialog implements SimulatorObserver{
 		return _status;
 	}
 
-	@Override
-	public void onAdvance(Map<String, BodiesGroup> groups, double time) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onReset(Map<String, BodiesGroup> groups, double time, double dt) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onRegister(Map<String, BodiesGroup> groups, double time, double dt) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onGroupAdded(Map<String, BodiesGroup> groups, BodiesGroup g) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onBodyAdded(Map<String, BodiesGroup> groups, Body b) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onDeltaTimeChanged(double dt) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onForceLawsChanged(BodiesGroup g) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
