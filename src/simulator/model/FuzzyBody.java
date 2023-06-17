bdd995467138328988e4dd2772bdde7ecaaac487
LCM03 <lcabal03@ucm.es>
2023-06-17 17:26:08 +0200

    FuzzyBody
    class

--- src/simulator/model/FuzzyBody.java
+++ src/simulator/model/FuzzyBody.java
@@ -2,15 +2,19 @@ package simulator.model;
 
 import simulator.misc.Vector2D;
 
-public class FuzzyBody extends Body{
-	private double pr;
-	private double d;
-	private double n;
+public class FuzzyBody extends MovingBody {
 
-	public FuzzyBody(String id, String gid, Vector2D v, Vector2D p, double m, double pr, double d, double n) {
+	private boolean fuzzy = false;
+	private double pr, d;
+	private int n, stepsInFuzzyMode = 0;
+	private Vector2D initialVelocity = new Vector2D();
+
+	public FuzzyBody(String id, String gid, Vector2D p, Vector2D v, double m, double pr, double d, int n) {
 		super(id, gid, v, p, m);
-		if(pr < 0 || pr > 1) // Igual con d y con n.
-			throw new IllegalArgumentException("One of the parameters of your FuzzyBody is invalid");
+		if (pr <= 0 || pr >= 1 || d <= 0.5 || d >= 1 || n <= 1) {
+			throw new IllegalArgumentException("One of the parameters of your Body is invalid");
+		}
+		initialVelocity = v;
 		this.pr = pr;
 		this.d = d;
 		this.n = n;
@@ -18,14 +22,30 @@ public class FuzzyBody extends Body{
 
 	@Override
 	void advance(double dt) {
-		
-		
-	}
-	
-	@Override
-	void addForce(Vector2D fNew) { //package protected
-		//f = f.scale(d);
-		// Overridear para que la fuerza en este body se aplique de manera distinta a como se aplica en el resto.
+		if (!fuzzy) {
+			super.advance(dt);
+			if (Math.random() < pr) {
+				v = initialVelocity;
+				fuzzy = true;
+				stepsInFuzzyMode = n;
+			}
+		} else {
+			super.advance(dt);
+			if (Math.random() < pr) {
+				v = v.rotate(1);
+			}
+			stepsInFuzzyMode--;
+			if (stepsInFuzzyMode <= 0) {
+				fuzzy = false;
+			}
+
+		}
 	}
 
+	@Override
+	void addForce(Vector2D fNew) {
+		if (fuzzy && Math.random() < pr)
+			f = f.scale(d);
+		super.addForce(fNew);
+	}
 }
