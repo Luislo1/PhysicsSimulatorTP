333a62794d4f705d9d95a4ced6344a4106fc142f
LCM03 <lcabal03@ucm.es>
2023-06-18 10:48:16 +0200

    BatchModeNOption

--- src/simulator/model/BatchModeNOption.java
+++ src/simulator/model/BatchModeNOption.java
@@ -10,55 +10,56 @@ public class BatchModeNOption implements SimulatorObserver{
 	   -i resources/examples/input/ex1.json -o resources/examples/expected_output/out.1.json -s 1000 -dt 1000 -fl nlug -m batch -n
 	*/
 	
-	Map<Body, Integer> numberOfNorth;
-	Map<Body, Boolean> needsToChange;
+	private Map<String, Integer> numberOfNorth;
+	private Map<String, Boolean> needsToChange;
 	
 	public BatchModeNOption(Controller ctrl) {
-		numberOfNorth = new HashMap<Body, Integer>();
-		needsToChange = new HashMap<Body, Boolean>();
 		ctrl.addObserver(this); // Toda clase que extienda simulatorObserver hay que añadirla a la lista de observers, sino no puede coger la informacion.
+		numberOfNorth = new HashMap<>();
+		needsToChange = new HashMap<>();
 	}
 	
 	
 	@Override
 	public void onAdvance(Map<String, BodiesGroup> groups, double time) {
-		Integer valueOfBody;
 		for (Map.Entry<String, BodiesGroup> entry : groups.entrySet()) {
 			BodiesGroup value = entry.getValue();
 			for(Body b: value) {
-				if(b.getVelocity().getY() < 0) { // If it's negative.
-					needsToChange.put(b, true);
-				}
-				else
-					needsToChange.put(b, false);
+				String id = b.getId() + ":" + b.getgId();
+				Double velY = b.getVelocity().getY();
 				
-				if(numberOfNorth.containsKey(b)) { // If the key is already in the map.
-					if(needsToChange.get(b).equals(true)) {
-						valueOfBody = numberOfNorth.get(b) + 1;
-						numberOfNorth.replace(b, valueOfBody);
-					}
-				}
-				else { // Create the key if it's not yet in the map.
-					if(needsToChange.get(b).equals(true))
-						numberOfNorth.put(b, 1);
-					else
-						numberOfNorth.put(b, 0);
-			    }
-			
+				if (velY > 0 && needsToChange.get(id)) {
+					needsToChange.put(id, false);
+					int newAmount = numberOfNorth.get(id);
+					newAmount++;
+					numberOfNorth.put(id, newAmount);
+				} 
+				
+				else if (velY < 0) 
+					needsToChange.put(id, true);	
 			}
 		}
 	}
 
 	@Override
-	public void onReset(Map<String, BodiesGroup> groups, double time, double dt) {
-		// TODO Auto-generated method stub
-		
+	public void onReset(Map<String, BodiesGroup> groups, double time, double dt) { // Si reseteo el simulator.
+		numberOfNorth.clear();
 	}
 
 	@Override
-	public void onRegister(Map<String, BodiesGroup> groups, double time, double dt) {
-		// TODO Auto-generated method stub
-		
+	public void onRegister(Map<String, BodiesGroup> groups, double time, double dt) { // Si añado ??????
+		for (Map.Entry<String, BodiesGroup> entry : groups.entrySet()) {
+			BodiesGroup value = entry.getValue();
+			for (Body b : value) {
+				String id = b.getId() + ":" + b.getgId();
+				boolean pointsNorth = false;
+				numberOfNorth.put(id, 0);
+				if (b.getVelocity().getY() < 0) {
+					pointsNorth = true;
+				}
+				needsToChange.put(id, pointsNorth);
+			}
+		}
 	}
 
 	@Override
@@ -69,8 +70,13 @@ public class BatchModeNOption implements SimulatorObserver{
 
 	@Override
 	public void onBodyAdded(Map<String, BodiesGroup> groups, Body b) {
-		// TODO Auto-generated method stub
-		
+		String id = b.getId() + ":" + b.getgId();
+		boolean pointsNorth = false;
+		numberOfNorth.put(id, 0);
+		if (b.getVelocity().getY() < 0) {
+			pointsNorth = true;
+		}
+		needsToChange.put(id, pointsNorth);
 	}
 
 	@Override
@@ -86,10 +92,9 @@ public class BatchModeNOption implements SimulatorObserver{
 	}
 	
 	public void PrintNumberOfNorth() {
-		
-		for(Map.Entry<Body, Integer> entry: numberOfNorth.entrySet()) {
-				Integer value = entry.getValue();
-				System.out.println(entry.getKey().getId() + ":" + entry.getKey().getgId() + " " + value);
+		System.out.println("Turn to North Statistics:");
+		for(Map.Entry<String, Integer> entry: numberOfNorth.entrySet()) {
+				System.out.println(entry.getKey() + " => " + String.valueOf(entry.getValue()));
 		}
 	}
 
