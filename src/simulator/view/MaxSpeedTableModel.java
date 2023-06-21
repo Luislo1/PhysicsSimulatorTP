e714f4344e44fb16f4e9134c4552fca3ce76b6f0
LCM03 <lcabal03@ucm.es>
2023-06-21 18:00:00 +0200

    MaxSpeedTableModel

--- src/simulator/view/MaxSpeedTableModel.java
+++ src/simulator/view/MaxSpeedTableModel.java
@@ -1,6 +1,7 @@
 package simulator.view;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.swing.table.AbstractTableModel;
@@ -12,73 +13,123 @@ import simulator.model.SimulatorObserver;
 
 public class MaxSpeedTableModel extends AbstractTableModel implements SimulatorObserver{
 	String[] _header = { "Time", "Id", "gId", "Max Speed" };
+	private List<Body> _bodies;
+	private List<Double> _time;
+	private List<Double> _maxSpeedList;
+	private int _steps;
 	
-	MaxSpeedTableModel(Controller ctrl, int steps) {
+	MaxSpeedTableModel(Controller ctrl, int steps){
+		_bodies = new ArrayList<>();
+		_time = new ArrayList<>();
+		_maxSpeedList = new ArrayList<>();
+		_steps = steps;
 		ctrl.addObserver(this);
-	}
-
-	@Override
-	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 	
 	public String getColumnName(int col) {
 		return _header[col];
 	}
-
-
+	
 	@Override
-	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+	public int getRowCount() {
+		return _maxSpeedList == null ? 0 : _maxSpeedList.size();
 	}
 
 	@Override
-	public void onAdvance(Map<String, BodiesGroup> groups, double time) {
-		// TODO Auto-generated method stub
-		
+	public int getColumnCount() {
+		return _header.length;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Object s = null;
+		Body b = _bodies.get(rowIndex);
+		switch (columnIndex) {
+		case 0:
+			s = String.valueOf(_time.get(rowIndex));
+			break;
+		case 1:
+			s = b.getId();
+			break;
+		case 2:
+			s = b.getgId();
+			break;
+		case 3:
+			s = _maxSpeedList.get(rowIndex);
+			break;
+		default:
+			break;
+		}
+		return s;
+	}
+	
+	public void reset(int initialSteps) {
+		_bodies.clear();
+		_time.clear();
+		_maxSpeedList.clear();
+		_steps = initialSteps;
+	}
+	
+	private void removeFirst() { // Para hacer que siempre haya N simulation steps cuerpos o menos en la tabla y nunca más.
+		_bodies.remove(0);
+		_time.remove(0);
+		_maxSpeedList.remove(0);
+	}
+
+	@Override
+	public void onAdvance(Map<String, BodiesGroup> groups, double time) { // En cada step busco el body con mayor velocidad de todos y esa velodidad la guardo.
+		Double maxSpeed = 0.0;
+		Body maxBody = null;
+		for (Map.Entry<String, BodiesGroup> entry : groups.entrySet()) {
+			BodiesGroup value = entry.getValue();
+			for (Body b : value) {
+				Double currVel = b.getVelocity().magnitude();
+				if (currVel > maxSpeed) {
+					maxSpeed = currVel;
+					maxBody = b;
+				}
+			}
+		}
+		_time.add(time);
+		_bodies.add(maxBody);
+		_maxSpeedList.add(maxSpeed);
+		if(_bodies.size() == _steps + 1) { 
+			removeFirst();
+		}
+		fireTableDataChanged();
 	}
 
 	@Override
 	public void onReset(Map<String, BodiesGroup> groups, double time, double dt) {
-		// TODO Auto-generated method stub
-		
+		_time.clear();
+		_bodies.clear();
+		_maxSpeedList.clear();
+		fireTableStructureChanged();
 	}
 
 	@Override
-	public void onRegister(Map<String, BodiesGroup> groups, double time, double dt) {
-		// TODO Auto-generated method stub
-		
+	public void onRegister(Map<String, BodiesGroup> groups, double time, double dt) { // Pq en el onRegister y los de abajo solo pongo fireTableStructureChanged?
+		fireTableStructureChanged();
 	}
 
 	@Override
 	public void onGroupAdded(Map<String, BodiesGroup> groups, BodiesGroup g) {
-		// TODO Auto-generated method stub
-		
+		fireTableStructureChanged();
 	}
 
 	@Override
 	public void onBodyAdded(Map<String, BodiesGroup> groups, Body b) {
-		// TODO Auto-generated method stub
+		fireTableStructureChanged();
 		
 	}
 
 	@Override
 	public void onDeltaTimeChanged(double dt) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void onForceLawsChanged(BodiesGroup g) {
-		// TODO Auto-generated method stub
 		
 	}
 
