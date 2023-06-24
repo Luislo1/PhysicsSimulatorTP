7da064ed2281e33260d37ba01006500aff6221f7
LCM03 <lcabal03@ucm.es>
2023-06-24 22:36:37 +0200

    c

--- src/simulator/view/MainWindow.java
+++ src/simulator/view/MainWindow.java
@@ -44,6 +44,8 @@ public class MainWindow extends JFrame {
 		JPanel bodiesTable = new InfoTable("Bodies", new BodiesTableModel(_ctrl));
 		bodiesTable.setPreferredSize(new Dimension(500, 250));
 		contentPanel.add(bodiesTable);
+		
+		JPanel massLossingTextArea 
 
 		addWindowListener(new WindowListener() {
 
